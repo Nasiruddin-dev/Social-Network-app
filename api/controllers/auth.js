02bec29f02c1b18a8c2f520d462423a2d385f171
Nasiruddin-dev <noorbashanasiruddin@gmail.com>
2025-10-20 16:20:19 +0530

    added expiry time of jwt token

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -47,7 +47,7 @@ export const login = (req, res) => {
     if (!checkPassword)
       return res.status(400).json("Wrong password or username!");
 
-    const token = jwt.sign({ id: data[0].id }, "secretkey");
+  const token = jwt.sign({ id: data[0].id }, "secretkey", { expiresIn: "7d" });
 
     const { password, ...others } = data[0];
 
